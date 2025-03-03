2df2a65df7a076cb4ef5a926cb73895fd4fd3f1d
ineslidorio12 <ineslidoorio@gmail.com>
2025-03-03 16:45:40 +0000

    criacao de ambiente virtual com flet, e inicio do desenvolvimento da aplicação

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.add(ft.Text(value="Hello, world!"))
+    page.title="TO DO LIST"
     
-ft.app(main)
+    def add_clicked(e):
+            tasks_view.controls.append(ft.Checkbox(label=new_task.value))
+            new_task.value = ""
+            page.update()
+
+    new_task = ft.TextField(hint_text="O QUE QUERES FAZER?", expand=True)
+    tasks_view = ft.Column()
+    
+    view=ft.SafeArea(
+    ft.Column(
+        width=500,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_task, 
+                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked)
+                ],
+            ),
+            tasks_view,
+        ],
+    ))
+    
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(view)
+    
+    page.update()
+    
+ft.app(target=main, view=ft.WEB_BROWSER)
