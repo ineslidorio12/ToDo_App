e71bb14d29974c2da862122086044ce55abe0d76
ineslidorio12 <ineslidoorio@gmail.com>
2025-03-05 19:31:55 +0000

    desenvolvimento de funçoes para task, como editar, e remover a task

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
 import flet as ft
+from task import Task
 
-def main(page: ft.Page):
-    page.title="TO DO LIST"
-    
-    def add_clicked(e):
-            tasks_view.controls.append(ft.Checkbox(label=new_task.value))
-            new_task.value = ""
-            page.update()
 
-    new_task = ft.TextField(hint_text="O QUE QUERES FAZER?", expand=True)
-    tasks_view = ft.Column()
-    
-    view=ft.SafeArea(
-    ft.Column(
-        width=500,
-        controls=[
-            ft.Row(
+class ToDoApp(ft.Column):
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        self.page = page
+        self.new_task = ft.TextField(hint_text="O que precisas fazer?", expand = True)
+        self.tasks_view = ft.Column()
+        self.view = ft.SafeArea(
+            ft.Column(
+                width=500,
                 controls=[
-                    new_task, 
-                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked)
+                    ft.Row(
+                        controls=[
+                            self.new_task, 
+                            ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=self.add_clicked)
+                        ],
+                    ),
+                    self.tasks_view,
                 ],
-            ),
-            tasks_view,
-        ],
-    ))
+            )
+        )
+        self.controls.append(self.view)
     
+    def add_clicked(self, e):
+        if self.new_task.value.strip():
+            task = Task(self.new_task.value, self.task_delete)
+            self.tasks_view.controls.append(task)
+            self.new_task.value = ""
+            self.page.update()
+        
+    def task_delete(self, task):
+        self.tasks_view.controls.remove(task)
+        self.page.update()
+        
+    
+def main(page: ft.Page):
+    page.title="TO DO LIST"        
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
-    
     page.update()
+    
+    todo = ToDoApp(page)
+    page.add(todo)
     
 ft.app(target=main, view=ft.WEB_BROWSER)
